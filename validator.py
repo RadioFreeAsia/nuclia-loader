bb877ed510dc19e1d6dbd29b2cdf2422cd03d59c
flipmcf <mcfaddenm@rfa.org>
2023-08-03 16:34:03 -0400

    this just counts stuff

---
+++ validator.py
@@ -0,0 +1,44 @@
+"""
+This loads a json data file exported from plone and:
+counts the number of json items  - to manually validate a good export.
+"""
+import argparse
+import ijson
+
+
+def process_args():
+    parser = argparse.ArgumentParser(description="Validate a plone export."
+                                                 "Note only 'story' types are expected",
+                                     usage="usage: validator.py <filename>")
+    parser.add_argument("filename",
+                        help="filename of json export")
+
+    parser.add_argument("-v", "--verbose",
+                        help="turn on debug")
+
+    parsed_args = parser.parse_args()
+
+    return parsed_args
+
+
+def validate(filename):
+    with open(filename, 'r') as filep:
+
+        # stream it from json into objects one item at a time
+        items = ijson.items(filep, 'item')
+        objects = 0
+        errors = 0
+
+        for item in items:
+            if '@id' in item:
+                objects += 1
+            else:
+                errors = len(item['unexported_paths'])
+
+    print(f"{filename}:  {objects} objects | {errors} errors")
+
+
+if __name__ == "__main__":
+    args = process_args()
+
+    validate(args.filename)
